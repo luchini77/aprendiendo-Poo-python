7fedf7f8fcb0449d09bf798184766124dcd38706
Luis Valenzuela <luchinilvp@gmail.com>
2018-11-26 14:50:06 -0300

    Primera mejora

--- caballeros.py
+++ caballeros.py
@@ -1,8 +1,34 @@
 class Caballeros:
-    poder=50
-    cosmos=50
-    nombre_constelacion="Nada"
-    hechizo="Puff"
 
-    def ataque(self):
-        return self.hechizo
+    cosmos="na"
+    velocidad_ataque = "lenteja"
+
+    def __init__(self):
+        self.nombre=input("Ingrese nombre del caballero: ")
+        self.nombre_constelacion=input("Ingrese la armadura: ")
+        self.ataque = input("Ingrese ataque del caballero: ")
+
+
+
+    def __str__(self):
+        return "{} es el caballero {}\nposee un cosmos del {}\ntiene una velocidad de ataque de {}\ny un ataque de nombre {}".format(
+            self.nombre,self.nombre_constelacion,self.cosmos,self.velocidad_ataque,self.ataque
+        )
+
+
+class Caballeros_Oro(Caballeros):
+
+    cosmos = "Septimo sentido"
+    velocidad_ataque = "Velocidad de la luz"
+
+
+class Caballeros_Plata(Caballeros):
+
+    cosmos = "medio pelo"
+    velocidad_ataque = "Velocidad del sonido"
+
+
+class Caballeros_Bronce(Caballeros):
+
+    cosmos = "rascas"
+    velocidad_ataque = "Menos de la velocidad del sonido"
--- principal.py
+++ principal.py
@@ -1,33 +1,11 @@
-from caballeros import Caballeros
+import caballeros
 
-print("El caballero tiene: ")
+sagitario = caballeros.Caballeros_Oro()
+cobra = caballeros.Caballeros_Plata()
+pegaso = caballeros.Caballeros_Bronce()
 
-print(Caballeros.poder)
-print(Caballeros.cosmos)
-print(Caballeros.nombre_constelacion)
-
-print("El caballero 1 tiene: ")
-
-caballero=Caballeros()
-caballero.poder=40
-caballero.cosmos=50
-caballero.nombre_constelacion="Pegaso"
-caballero.hechizo="Meteoro Pegaso"
-
-print(caballero.poder)
-print(caballero.cosmos)
-print(caballero.nombre_constelacion)
-print(caballero.ataque())
-
-print("El caballero 2 tiene: ")
-
-caballero2=Caballeros()
-caballero2.poder=70
-caballero2.cosmos=80
-caballero2.nombre_constelacion="Cobra"
-caballero2.hechizo="La garra de la cobra"
-
-print(caballero2.poder)
-print(caballero2.cosmos)
-print(caballero2.nombre_constelacion)
-print(caballero2.ataque())
+print(sagitario)
+print()
+print(cobra)
+print()
+print(pegaso)
